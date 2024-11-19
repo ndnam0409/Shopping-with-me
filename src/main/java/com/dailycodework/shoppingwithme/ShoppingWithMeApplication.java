d95570ff853dfa8f66128e652ea44f43ac772473
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2024-11-19 22:30:40 +0700

    initialize project

---
+++ src/main/java/com/dailycodework/shoppingwithme/ShoppingWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.shoppingwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailycodework/shoppingwithme/ShoppingWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.shoppingwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
